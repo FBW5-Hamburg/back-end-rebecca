e87dae33f5ba4533596ff1c1b028226d0ad151ce
rebecca-dci <arowosokirebecca@gmail.com>
2020-08-17 21:07:15 +0200

    adminlogin

--- MainProject/app.js
+++ MainProject/app.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const path = require('path')
 const session = require('express-session')
+const fileupload = require('express-fileupload')
+
+//include dataModule
+const dataModule = require('./modules/dbmongoose')
 
 const app = express()
 
-//
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-const sessionOptions = {
-    secret: 'bookstore',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {} 
-}
-app.use(session(sessionOptions))
-
-//peblic folder url
+// public folder url
 app.use(express.static(path.join(__dirname,'public')))
 
 //set view engine
 app.set('view engine','ejs')
 app.set('views','views')
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+const sessionOptions = {
+    secret: 'vegefoods',
+    resave: false,
+    saveUninitialized: false, 
+}
+app.use(session(sessionOptions))
+
+app.use(fileupload({
+    limits: { fileSize: 50 * 1024 * 1024}
+}))
 
 
 // get main
@@ -44,7 +49,12 @@ app.get('/contact',(req, res) => {
 
 // get contact
 app.get('/admin',(req, res) => {
-    res.render('admin')
+    if(req.session.user) {
+        res.render('admin')
+    }else{
+        res.redirect('admin-login')
+    }
+  
 })
 
 // get admin login
@@ -57,16 +67,44 @@ app.post('/admin-login',(req, res) => {
     console.log(req.body)
 
     if(req.body.useremail =="rebecca@gmail.com" && req.body.userpassword =="123456"){
-       
+        console.log(1)
         req.session.user= {email:req.body.useremail , password:req.body.userpassword }
         res.json(1);
     }else{
+         console.log(2)
         req.session.user= null;
         res.json(2);
     }
 
 })
 
+// post add item
+app.post('/add-item',(req, res) => {
+   if(!req.session.user){
+    res.json(3);
+   }else if (req.files && Object.keys(req.files).length > 1 &&req.body.itemname && req.body.itemprice){
+
+    const itemname = req.body.itemname
+    const itemprice = req.body.itemprice
+ 
+
+        const imgs = []
+        for (const key in req.files){
+                imgs.push(req.files[key])
+        }
+        dataModule.addItem(itemname, itemprice,  imgs).then(() => {
+            res.json(1)
+        }).catch(error => {
+
+                res.json(2)
+            
+        })
+    
+}else{
+    res.json(2)
+}
+
+})
 
 app.listen(4500,() => {
     console.log('App listening on port 4500!')
--- MainProject/modules/dbmongoose.js
+++ MainProject/modules/dbmongoose.js
@@ -3,9 +3,29 @@ const passwordHash = require('password-hash')
 
 
 const connectionString = 'mongodb+srv://project:qwer123@cluster0.lh6tv.mongodb.net/nodejs?retryWrites=true&w=majority'
-const usersSchema = new usersSchema({})
 
-const Users = mongoose.model('users', usersSchema)
+// get schema object
+const Schema = mongoose.Schema
+//const {Schema} = mongoose
+
+const itemsSchema = new Schema({
+    itemname: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    itemprice: {
+        type: Number,
+        required: true
+    }
+    ,
+    itemimage: {
+        type: Number,
+        required: true
+    }
+})
+
+const Items = mongoose.model('items', itemsSchema)
 function connection(){
     return new Promise((resolve, reject) => {
         if (mongoose.connection.readyState === 1){
@@ -23,6 +43,54 @@ function connection(){
         }
     })
 }
+
+
+
+function addItem(itemname, itemprice, itemimage){
+    return new Promise((resolve, reject) =>{
+        connect().then(() => {
+            Items.findOne({itemname: itemname}).then(findItem => {
+                
+                if(findItem){
+                 
+                 reject(3)
+                    
+                } else {
+
+                    //create images array to be saved in database
+                    const imgsArr = []
+                    itemimage.forEach((img, idx)=> {
+                        // get file extension
+                        let ext = img.name.substr(img.name.lastIndexOf('.'))
+                        //set the new image name
+                        let newName = bookTitle.trim().replace(/ /g,'_')+ '_' + userid + '_' + idx + ext
+                        img.mv('./public/uploadedfiles/' + newName)
+                        imgsArr.push('/uploadedfiles/' + newName)
+                    })
+
+                    const newItem = new Items({
+                        itemname: itemname,
+                        itemprice: itemprice,
+                        itemimage: imgsArr
+                    })
+                    newItem.save().then(() => {
+                        resolve()
+                    }).catch(error => {
+                        reject(error)
+                    })
+                }
+            }).catch(error => {
+                reject(error)
+            })
+        }).catch(error => {
+            reject(error)
+        })
+        
+    })
+    
+       
+}
+
 module.exports = {
-    registration
+    addItem
 }
